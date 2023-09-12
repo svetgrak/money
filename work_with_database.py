c9de0f99ba514f99134c49332d610301629bf701
it30 <it30@bsmp.kras.rf>
2023-09-12 15:02:58 +0700

    123

--- work_with_database.py
+++ work_with_database.py
@@ -15,35 +15,36 @@ class WorkWithPostgreSQL(object):
     table_name = "budget_data"
     connection = ""
 
+    def __init__(self):
+        self.open_connection_database()
+
     # подключение к postgreSQL (возвращает подключение)
     def open_connection(self):
         try:
-            connection = psycopg2.connect(user=self.connect_user, password=self.connect_password,
+            self.connection = psycopg2.connect(user=self.connect_user, password=self.connect_password,
                                           host=self.connect_host, port=self.connect_port)
             print("Соединение c PostgreSQL установлено")
-            connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            self.connection = connection
+            self.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
         except (Exception, Error) as error:
             print("Ошибка при работе с PostgreSQL", error)
 
-        return self.get_connection()
+        return self.connection
 
     # закрыть любое подключение (передать подключение)
-    def close_connection(self, connection):
+    def close_connection(self):
         try:
-            connection.close()
+            self.connection.close()
             print("Соединение с PostgreSQL закрыто")
 
         except (Exception, Error) as error:
             print("Ошибка при работе с PostgreSQL", error)
 
     #создание базы данных budget_project, передать подключение к postgreSQL
-    def create_database(self, connection_postresql):
+    def create_database(self):
         try:
-            connection_postresql.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-            cursor = connection_postresql.cursor()
+            self.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            cursor = self.connection.cursor()
             sql_create_database = 'CREATE DATABASE ' + self.database_name
             cursor.execute(sql_create_database)
             print("База данных " + self.database_name + " успешно создана!")
@@ -54,10 +55,9 @@ class WorkWithPostgreSQL(object):
         return
 
     # удалить базу данных (передать имя базы данных и подключение к postgreSQL)
-    def delete_database(self, connection_postgresql):
+    def delete_database(self):
         try:
-
-            cursor = connection_postgresql.cursor()
+            cursor = self.connection.cursor()
             sql_delete_database = 'DROP DATABASE ' + self.database_name
             cursor.execute(sql_delete_database)
             print("База данных " + self.database_name + " удалена")
@@ -69,46 +69,42 @@ class WorkWithPostgreSQL(object):
     # подключение к базе данных по имени database_name (возвращает подключение)
     def open_connection_database(self):
         try:
-            connection = psycopg2.connect(user=self.connect_user, password=self.connect_password,
+            self.connection = psycopg2.connect(user=self.connect_user, password=self.connect_password,
                                           host=self.connect_host, port=self.connect_port,
                                           dbname=self.database_name)
 
             print("Соединение c базой " + self.database_name + " установлено")
-            connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            self.connection = connection
+            self.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
         except (Exception, Error) as error:
             print("Ошибка при работе с PostgreSQL", error)
 
-        return self.get_connection()
+        return self.connection
 
     # создать таблицу (передать подключение к базе данных)
-    def create_table(self, connection_database):
-
+    def create_table(self):
         try:
-
-            cursor_database = connection_database.cursor()
+            cursor_database = self.connection.cursor()
             create_table_query = '''CREATE TABLE ''' + self.table_name + ''' ( 
-                                id bigint NOT NULL,
+                                id bigint PRIMARY KEY,
                                 tag text NOT NULL,
                                 day date NOT NULL,
-                                money bigint NOT NULL,
+                                money int NOT NULL,
                                 category text NOT NULL,
-                                comment text,
-                                CONSTRAINT budget_data_pkey PRIMARY KEY (id)
+                                comment text
                                 )'''
             cursor_database.execute(create_table_query)
             print("Таблица " + self.table_name + " успешно создана!")
-            connection_database.commit()
+            self.connection.commit()
 
         except (Exception, Error) as error:
             print("Ошибка при работе с PostgreSQL", error)
 
 
     # очистить таблицу (передать подключение к базе данных)
-    def truncate_table(self, connection_database):
+    def truncate_table(self):
         try:
-            cursor_database = connection_database.cursor()
+            cursor_database = self.connection.cursor()
             query_truncate_table = "TRUNCATE " + self.table_name
 
             cursor_database.execute(query_truncate_table)
@@ -118,9 +114,9 @@ class WorkWithPostgreSQL(object):
             print("Ошибка при работе с PostgreSQL", error)
 
     # удалить таблицу
-    def delete_table(self, connection_database):
+    def delete_table(self):
         try:
-            cursor_database = connection_database.cursor()
+            cursor_database = self.connection.cursor()
             query_truncate_table = "DROP TABLE " + self.table_name
 
             cursor_database.execute(query_truncate_table)
@@ -129,24 +125,32 @@ class WorkWithPostgreSQL(object):
         except(Exception, Error) as error:
             print("Ошибка при работе с PostgreSQL", error)
 
-    def get_connection(self):
-        return self.connection
+    #добавить транзакцию в таблицу
+    def insert_transaction_in_table(self, data_insert):
+        try:
+            cursor_database = self.connection.cursor()
+            query_insert_in_table = ("INSERT INTO " + self.table_name +
+                                     " VALUES ('+', '2023-09-12', 100, 'питание работа', 'энергетос')")
+            cursor_database.execute(query_insert_in_table)
+            print("Данные добавлены")
+            self.connection.commit()
+        except(Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+
+    #удалить транзакцию из таблицы
 
 
 psql = WorkWithPostgreSQL()
-connection_psql = psql.open_connection()
-psql.create_database(connection_psql)
-psql.close_connection(connection_psql)
+psql.close_connection()
+psql.open_connection()
+psql.create_database()
+psql.close_connection()
 
-connection_db = psql.open_connection_database()
-psql.create_table(connection_db)
-psql.truncate_table(connection_db)
-psql.delete_table(connection_db)
-psql.close_connection(connection_db)
-
-connection_psql = psql.open_connection()
-psql.delete_database(connection_psql)
-psql.close_connection(connection_psql)
+psql.open_connection_database()
+psql.delete_table()
+psql.create_table()
+psql.insert_transaction_in_table('123')
+psql.close_connection()
 
 
 
